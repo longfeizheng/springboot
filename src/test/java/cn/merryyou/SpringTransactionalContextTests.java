e2c0b9035b7c312f75f0c51c8571c3274d22b9d0
ctsi <longfei_zheng@163.com>
2016-09-18 10:38:40 +0800

    添加自定义banner 和日志管理(彩色日志)

--- src/test/java/cn/merryyou/SpringTransactionalContextTests.java
+++ src/test/java/cn/merryyou/SpringTransactionalContextTests.java
@@ -1,24 +1,24 @@
-package cn.merryyou;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.sql.DataSource;
-
-@WebAppConfiguration
-@Transactional
-@SpringApplicationConfiguration(Application.class)
-public class SpringTransactionalContextTests extends AbstractTransactionalJUnit4SpringContextTests {
-
-	protected DataSource dataSource;
-
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		super.setDataSource(dataSource);
-		this.dataSource = dataSource;
-	}
-	
-}
+//package cn.merryyou;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import javax.sql.DataSource;
+//
+//@WebAppConfiguration
+//@Transactional
+//@SpringApplicationConfiguration(Application.class)
+//public class SpringTransactionalContextTests extends AbstractTransactionalJUnit4SpringContextTests {
+//
+//	protected DataSource dataSource;
+//
+//	@Autowired
+//	public void setDataSource(DataSource dataSource) {
+//		super.setDataSource(dataSource);
+//		this.dataSource = dataSource;
+//	}
+//
+//}
